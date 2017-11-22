9d9ccd10bb7c607b60de8abb7919784691c9b12f
Unknown <dibstern@gmail.com>
2017-11-22 18:26:56 +1100

    Monolithic Program Update
    - This supports *only* multiple degree holders
    - Emails results to self instead of a unimelb email address.
    - Creates a 'status' file, to allow the user to view the date of last update.
    - Retries click through (max 20 times) to wait for page load.
    - Documented
    - `system_test.py` provided to allow testing of libraries, phantomJS installation, creates a test email, and sends it.

--- results/main.py
+++ results/main.py
@@ -1,129 +1,270 @@
 """
-log in to my.unimelb results page using an externally-specified student
+-------------------------------------------------------------------------------
+Log in to my.unimelb results page using an externally-specified student
 account, check for updates on the results page, and then send the student
 an email if anything has changed
+
+Script originally written by Matthew Farrugia (GITHUB HERE)
+Forked & Refactored by D. Stern from 7/11/17-onwards
+Updates listed in the README.
+-------------------------------------------------------------------------------
+Current PID: 1392
+-------------------------------------------------------------------------------
 """
 
-# web stuff
-from selenium import webdriver
-from bs4 import BeautifulSoup
+# Reporting Back To User
+from time import localtime, strftime, sleep
+import random
 
-# email stuff
+# Web Navigation & Parsing
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+import re
+from contextlib import suppress
+
+# Email Creation & Sending
 import smtplib
 from email.mime.text import MIMEText
+
+QUERY = "?f=%24S1.EST.RSLTDTLS.WEB"
+URL = "https://prod.ss.unimelb.edu.au/student/SM/ResultsDtls10.aspx" + QUERY
+USERNAME_BOX_ID = "ctl00_Content_txtUserName_txtText"
+PASSWORD_BOX_ID = "ctl00_Content_txtPassword_txtText"
+LOGIN_BUTTON_ID = "ctl00_Content_cmdLogin"
+LOGOUT_BUTTON_ID = "ctl00_LogoutLinkButton"
+WAM_CLASS = "UMWAMText"
+
+# Specific to D. Stern (Multiple Degrees means an intermediate page & button)
+ENG_BUTTON_ID = "ctl00_Content_grdResultPlans_ctl04_ctl00"
+
+EMAIL_FILE = "email.txt"
+LOGIN_FILE = "login.txt"
+WAM_FILE = "wam.txt"
+STATUS_FILE = "status.txt"
+
+EMAIL_SUBJECT = "WAM Update"
+EMAIL_DOMAIN = "@gmail.com"
+
+CHANGE_MSG = "Hey, there!\n\nI noticed that your wam has changed from "
+SUCCESS_MSG = "Congratulations! "
+COMMISERATIONS_MSG = ("That's okay, you tried your best, "
+                      "and that's all anyone can ask for! ")
+END_MSG = "\n\nHave a nice day!\n\nLove, results robot\n--\n"
 
 
 def main():
 
-	# get login details from configuration file
-	username, password = get_username_password("login.txt")
+    # get login details from configuration file
+    username, password = get_username_password(LOGIN_FILE)
 
-	# login to results page and get its source
-	source = get_results(username, password)
+    # login to results page and retrieve the WAM and date
+    new_wam, new_date = get_wam_and_date(username, password)
 
-	# parse results page to check for updates
-	soup = BeautifulSoup(source, "html5lib")
+    # Check if WAM changed
+    changed, old_wam = has_changed(new_wam, new_date)
+    date_time_of_check = strftime("%a, %I:%M:%S %p, %d/%b/%Y", localtime())
 
-	# get the WAM
-	new_wam = float(soup.find(class_="UMWAMText").find("b").text)
+    # Create & send an email if anything changed
+    if changed:
 
-	# has it changed?
-	changed = False
-	with open("wam.txt", 'r+') as f:
-		
-		# what was the old wam?
-		old_wam = float(f.readline().strip())
-		
-		# update the file if it has changed
-		if (new_wam != old_wam):
-			changed = True
-			f.seek(0)
-			f.write(str(new_wam)+'\n')
-			f.truncate()
+        # Write the message and get the destination address
+        message = write_msg(old_wam, new_wam)
+        email_user, _pass = get_username_password(EMAIL_FILE)
+        target = email_user + EMAIL_DOMAIN
+
+        # Send off the message!
+        send_results(target, EMAIL_SUBJECT, message)
+        print("Sent an email to " + target)
+        with open(STATUS_FILE, 'w') as f:
+            f.write(f"Updated at {date_time_of_check}\n")
+
+    # 20% of the time, print update messages to terminal
+    elif random.random() < 0.20:
+            print(f"No update at: {date_time_of_check}")
 
 
-	# formulate and send an email if anything changed
-	if changed:
+def get_wam_and_date(username, password):
+    """Retrieve the WAM and the date of last update from my.unimelb.edu.au.
 
-		# construct the message body
-		message = "Hey, there!\n\n" \
-				+ "I noticed that your wam has changed from " \
-				+ str(old_wam) + " to " + str(new_wam) + ". "
-		if(new_wam > old_wam):
-			message += "Congratulations! "
-		else:
-			message += "That's okay, you tried your best, " \
-					+ "and that's all anyone can ask for! "
-		message += "\n\nHave a nice day!\n\nLove, results robot\n--\n"
+    Args:
+        username (str): The my.unimelb.edu.au username.
+        password (str): The my.unimelb.edu.au password
 
-		# also the subject and destination address
-		subject = "WAM Update to " + str(new_wam)
-		target = username + "@student.unimelb.edu.au"
+    Return:
+        float: The WAM found at my.unimelb.edu.au.
+        str  : The date of last update found at my.unimelb.edu.au.
+    """
 
-		# and send off the message!
-		send_results(target, subject, message)
-		print "Sent an email to " + target
+    # login to results page and get its source
+    source = get_results(username, password)
 
+    # parse results page to check for updates
+    soup = BeautifulSoup(source, "html5lib")
+
+    # Get the WAM object
+    wam_div = soup.find(class_=WAM_CLASS)
+
+    # Get the WAM and date
+    wam = float(wam_div.find("b").text)
+    date = (re.findall(r'\d{2}-[a-zA-Z]{3}-\d{4}', wam_div.__str__()))[0]
+
+    return wam, date
+
+
+def write_msg(old_wam, new_wam):
+    """Write the message for the email.
+
+    Args:
+        old_wam (str): The old WAM from the stored file.
+        new_wam (str): The new WAM from my.unimelb.edu.au
+
+    Return:
+        str: A message for the email.
+
+    """
+
+    # construct the message body
+    message = CHANGE_MSG + f"{old_wam} to {new_wam}."
+    if new_wam > old_wam:
+        message += SUCCESS_MSG
+    else:
+        message += COMMISERATIONS_MSG
+
+    return message + END_MSG
+
+
+def has_changed(new_wam, new_date):
+    """Checks if the new WAM is different to the old WAM.
+
+    Args:
+        new_wam (float): The new WAM retrieved from the site, to be compared to
+                         the old WAM.
+
+    Returns:
+        bool: True if the old WAM is different to new_wam, else False
+        float: The old WAM found in the file.
+
+    """
+
+    changed = False
+    with open(WAM_FILE, 'r+') as f:
+
+        # what was the old wam and date?
+        old_wam = float(f.readline().strip())
+        old_date = f.readline().strip()
+
+        # update the file if it has changed
+        if (new_wam != old_wam and new_date != old_date):
+            changed = True
+            f.seek(0)
+            f.write(str(new_wam)+'\n')
+            f.truncate()
+
+    return changed, old_wam
 
 
 def get_results(username, password):
-	"""
-	log in to the results page as username, get the page's HTML source,
-	log out of the page and then return the page source.
-	"""
+    """Log in to the results page, retrieve the page's HTML source & logout.
 
-	# load up the browser
-	driver = webdriver.PhantomJS()
-	driver.set_window_size(1120, 550)
+    Args:
+        username  (str): The username of the my.unimelb account.
+        password  (str): The password of the my.unimelb account.
 
-	# go to the results page and enter the username and password
-	url = "https://prod.ss.unimelb.edu.au/student/SM/ResultsDtls10.aspx" \
-		+ "?f=%24S1.EST.RSLTDTLS.WEB"
-	driver.get(url)
+    Returns:
+        str: The page source.
 
-	usernamebox = driver.find_element_by_id("ctl00_Content_txtUserName_txtText")
-	passwordbox = driver.find_element_by_id("ctl00_Content_txtPassword_txtText")
+    """
 
-	usernamebox.send_keys(username)
-	passwordbox.send_keys(password)
+    # load up the browser
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1120, 550)
 
-	loginbutton = driver.find_element_by_id("ctl00_Content_cmdLogin")
-	loginbutton.click()
+    # go to the results page and enter the username and password
+    driver.get(URL)
 
-	# great the source before we log out, then reurn the source
-	page_source = driver.page_source
+    usernamebox = driver.find_element_by_id(USERNAME_BOX_ID)
+    passwordbox = driver.find_element_by_id(PASSWORD_BOX_ID)
 
-	logoutbutton = driver.find_element_by_id("ctl00_LogoutLinkButton")
-	logoutbutton.click()
+    usernamebox.send_keys(username)
+    passwordbox.send_keys(password)
 
-	return page_source
+    loginbutton = driver.find_element_by_id(LOGIN_BUTTON_ID)
+    loginbutton.click()
+
+    # Navigate into Results page for Masters Degree
+    click_through_intermediate_page(driver)
+
+    # great the source before we log out, then reurn the source
+    page_source = driver.page_source
+
+    logoutbutton = driver.find_element_by_id(LOGOUT_BUTTON_ID)
+    logoutbutton.click()
+
+    return page_source
+
+
+def click_through_intermediate_page(driver):
+    '''Click on the correct button, through to the intermediate page.
+
+    Args:
+        driver (webdriver.PhantomJS()): The driver object.
+    '''
+    retries = 20
+    while retries > 0:
+        with suppress(NoSuchElementException):
+            sleep(2)
+            view_eng_button = driver.find_element_by_id(ENG_BUTTON_ID)
+            view_eng_button.click()
+            return True
+        retries -= 1
+    return False
 
 
 def send_results(target, subject, message):
-	"""log in to gmail as the results robot, and send an email to the target"""
+    """Login to gmail as the results robot, and send an email to the target
 
-	# what are the results robot's authentication details?
-	username, password = get_username_password("email.txt")
+    Args:
+        target  (str): The email to which the message will be sent.
+        subject (str): The subject of the email to be sent.
+        message (str): The message to be sent in the email.
 
-	sender = username + "@gmail.com"
-	
-	# make the email object
-	msg = MIMEText(message)
-	msg['To'] = target
-	msg['Subject'] = subject
-	msg['From'] = sender
+    """
 
-	# Log into gmail's free SMTP server as the
-	# results robot and send the message
-	s = smtplib.SMTP('smtp.gmail.com', 587); s.ehlo(); s.starttls()
-	s.login(sender, password)
-	s.sendmail(sender, [target], msg.as_string())
-	s.quit()
+    # what are the results robot's authentication details?
+    username, password = get_username_password(EMAIL_FILE)
+
+    sender = username + EMAIL_DOMAIN
+
+    # make the email object
+    msg = MIMEText(message)
+    msg['To'] = target
+    msg['Subject'] = subject
+    msg['From'] = sender
+
+    # Log into gmail's free SMTP server as the results robot & send the message
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.login(sender, password)
+    s.sendmail(sender, [target], msg.as_string())
+    s.quit()
+
 
 def get_username_password(filename):
-	with open(filename) as f:
-		username = f.readline().strip()
-		password = f.readline().strip()
-	return (username, password)
+    """Retrieves the stored username and password.
 
-if __name__ == '__main__': main()
+    Args:
+        filename (str): The name of the file in which the details are stored
+    Returns:
+        str: The username & password stored in the file.
+
+    """
+    with open(filename) as f:
+        username = f.readline().strip()
+        password = f.readline().strip()
+    return (username, password)
+
+
+if __name__ == '__main__':
+    main()
